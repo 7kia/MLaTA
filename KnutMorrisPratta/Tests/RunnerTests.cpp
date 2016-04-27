987a7a0f4a1c22acb3469b02ca0ccf4f6342c1d1
7kia <il7kia@yandex.ru>
2016-04-27 18:58:07 +0400

    Add tests.

---
+++ KnutMorrisPratta/Tests/RunnerTests.cpp
@@ -0,0 +1,91 @@
+#include "stdafx.h"
+#include "RunnerTests.h"
+
+BOOST_AUTO_TEST_SUITE(ApplicationTestModule)
+
+bool TestApplictation(const std::string & nameInputFile, const std::string & nameOutputFile)
+{
+	CRunner application(nameInputFile, nameOutputFile);
+
+	application.Run();
+
+	return true;
+}
+
+const std::string PATH_RIGHT_DATA = "Right\\";
+
+void CompareFiles(const std::string & first, const std::string & second)
+{
+	std::ifstream resultFile(first);
+	std::ifstream rightResultFile(second);
+
+	std::istream_iterator<char> iterResultFile(resultFile), endIter;
+	std::istream_iterator<char> iterRightRsultFile(rightResultFile);
+
+	BOOST_CHECK_EQUAL_COLLECTIONS(iterResultFile, endIter, iterRightRsultFile, endIter);
+};
+
+BOOST_AUTO_TEST_CASE(Throw_exception_for_nonexistent_file)
+{
+	BOOST_CHECK_THROW(TestApplictation("input-.txt", "output1.txt"), std::ifstream::failure);
+}
+
+BOOST_AUTO_TEST_CASE(Throw_exception_for_incorrect_arguments)
+{
+	BOOST_CHECK_THROW(TestApplictation("input0.txt", "output1.txt"), std::ifstream::failure);
+}
+
+BOOST_AUTO_TEST_CASE(small_test)
+{
+	std::string nameInputFile = "input1.txt";
+	std::string nameOutputFile = "output1.txt";
+	BOOST_CHECK(TestApplictation(nameInputFile, nameOutputFile));
+	CompareFiles(nameOutputFile, PATH_RIGHT_DATA + nameOutputFile);
+}
+
+BOOST_AUTO_TEST_CASE(a_lot_repeats)
+{
+	std::string nameInputFile = "input2.txt";
+	std::string nameOutputFile = "output2.txt";
+	BOOST_CHECK(TestApplictation(nameInputFile, nameOutputFile));
+	CompareFiles(nameOutputFile, PATH_RIGHT_DATA + nameOutputFile);
+}
+
+
+BOOST_AUTO_TEST_CASE(several_strings_in_file)
+{
+	std::string nameInputFile = "input3.txt";
+	std::string nameOutputFile = "output3.txt";
+	BOOST_CHECK(TestApplictation(nameInputFile, nameOutputFile));
+	CompareFiles(nameOutputFile, PATH_RIGHT_DATA + nameOutputFile);
+}
+
+BOOST_AUTO_TEST_CASE(string_not_found)
+{
+	std::string nameInputFile = "input4.txt";
+	std::string nameOutputFile = "output4.txt";
+	BOOST_CHECK(TestApplictation(nameInputFile, nameOutputFile));
+	CompareFiles(nameOutputFile, PATH_RIGHT_DATA + nameOutputFile);
+}
+
+/*
+BOOST_AUTO_TEST_CASE(Check_graph_with_several_loop)
+{
+	std::string nameInputFile = "input4.txt";
+	std::string nameOutputFile = "output4.txt";
+	BOOST_CHECK(TestApplictation(nameInputFile, nameOutputFile));
+	CompareFiles(nameOutputFile, PATH_RIGHT_DATA + nameOutputFile);
+}
+
+
+BOOST_AUTO_TEST_CASE(Check_big_test_2)
+{
+	std::string nameInputFile = "inputBig.txt";
+	std::string nameOutputFile = "outputBig.txt";
+	BOOST_CHECK(TestApplictation(nameInputFile, nameOutputFile));
+	CompareFiles(nameOutputFile, PATH_RIGHT_DATA + nameOutputFile);
+}
+*/
+
+
+BOOST_AUTO_TEST_SUITE_END()
