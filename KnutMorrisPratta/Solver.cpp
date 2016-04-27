f2c835315122ae86605e2ce21a8facdddbd7a873
7kia <il7kia@yandex.ru>
2016-04-27 18:05:36 +0400

    Add optimizations.

--- KnutMorrisPratta/Solver.cpp
+++ KnutMorrisPratta/Solver.cpp
@@ -32,6 +32,8 @@ PrefixFunction CSolver::GetPrefixFunction(const string & searchString, string in
 {
 	PrefixFunction result;
 
+	// TODO : delete
+	size_t counter = 0;
 	//inputString.insert(inputString.begin(), '#');
 	//inputString.insert(inputString.begin(), searchString.begin(), searchString.end());
 	result.resize(inputString.size(), 0);
@@ -41,9 +43,75 @@ PrefixFunction CSolver::GetPrefixFunction(const string & searchString, string in
 	for (size_t index = 1; index < inputString.size(); index++)
 	{
 		////////////////////
+		// First optimization
+
+		size_t startIndex = 1;
+		///*
+		if ((index - 1) != std::numeric_limits<size_t>::max())
+		{
+			if (result[index - 1] > 0)
+			{
+				startIndex += result[index - 1];
+			}
+		}
+		//*/
+
+		////////////////////
+		// Second optimization
+		if (startIndex != 1)
+		{
+			char lastInPrefix = inputString[startIndex - 1];
+			char lastInSuffix = inputString[index];
+
+			bool symbolsIsEqual = (lastInPrefix == lastInSuffix);
+
+			if (symbolsIsEqual)
+			{
+				result[index] += result[index - 1] + 1;
+				continue;
+			}
+		}
+		/*
+		for (size_t index2 = index - 1; (index2) >= startIndex; index2--)
+		{
+			counter++;// TODO : delete // 45 // 37
+
+			//prefix = inputString.substr(0, index2);
+			//suffix = inputString.substr(index - index2 + 1, index2);
+
+			char lastInPrefix = inputString[index2];
+			char lastInSuffix= inputString[index];
+
+			bool symbolsIsEqual = (lastInPrefix == lastInSuffix);
+
+
+			if (symbolsIsEqual)
+			{
+				result[index]++;
+			}
+
+			if (startIndex != 1)
+			{
+				result[index] += result[index - 1];
+				break;
+			}
+
+			/.*
+						if ((!suffix.empty() && !prefix.empty()) && (prefix == suffix))
+			{
+				result[index] = index2;
+				break;
+			}
+			*./
+
+
+		}
+		*/
+		////////////////////
 		// TODO n ^ 3
 		////////////////////
-		for (size_t index2 = 1; (index2)  < index; index2++)
+		///*
+		for (size_t index2 = startIndex; (index2)  < index; index2++)
 		{
 			prefix = inputString.substr(0, index2);
 			suffix = inputString.substr(index - index2 + 1, index2);
@@ -51,7 +119,10 @@ PrefixFunction CSolver::GetPrefixFunction(const string & searchString, string in
 			{
 				result[index] = index2;
 			}
+
+			counter++;// TODO : delete // 45 // 37 // 19
 		}
+		//*/
 		////////////////////
 	}
 	return result;// TODO
